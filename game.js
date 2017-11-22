6f1055fbed46cec36954ecdc5ae4e7edd82fc17a
Rob Tarr <Rob@robtarr.net>
2017-11-21 20:59:56 -0500

    initial commit

---
+++ game.js
@@ -0,0 +1,30 @@
+import { send } from './message';
+
+class Game {
+  constructor(ts) {
+    this.ts = ts;
+    this.players = [];
+  }
+
+  add(event) {
+    const { reaction, item, item_user } = event;
+
+    if (reaction === 'ping' && item.ts === this.ts) {
+      if (!this.players.includes(item_user)) {
+        this.players.push(item_user);
+      }
+    }
+
+    return this.players.length === 4;
+  };
+
+  remove(event) {
+    const { reaction, item, item_user } = event;
+
+    if (reaction === 'ping' && item.ts === this.ts) {
+      this.players = this.players.filter(player => player !== item_user);
+    }
+  }
+}
+
+export default Game;
---
+++ index.js
@@ -0,0 +1,40 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import Game from'./game';
+import { send } from './message';
+
+const app = express();
+let game = null;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => res.send('Four! Active'));
+
+app.post('/', (req, res) => {
+  if (req.body.challenge) return res.send(req.body.challenge);
+
+  let result = null;
+  const event = req.body.event;
+  console.log(event);
+
+  if (event) {
+    if (!game) game = new Game(event.item.ts);
+
+    if (event.type === 'reaction_added') {
+      result = game.add(event);
+    }
+    if (event.type === 'reaction_removed') {
+      game.remove(event);
+    }
+
+    console.log(`Players: ${game.players}\n`);
+
+    if (result) {
+      send(game.players, event.item.channel);
+      game = null;
+    }
+  }
+});
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
